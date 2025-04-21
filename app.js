8b863bd41c604ce18ea139d55bfa1c5ef4238ef6
Jogi Nayak <jogi@aisthetic.co>
2025-04-21 23:57:34 +0530

    add cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const enquiriesRouter = require('./routes/enquiries');
 
-var indexRouter = require('./routes/index');
-var enquiriesRouter = require('./routes/enquiries');
-
-var app = express();
+const app = express();
 const corsOptions = {
   origin: ["http://localhost:3000","https://nationaloutdoorschool.com"],
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
